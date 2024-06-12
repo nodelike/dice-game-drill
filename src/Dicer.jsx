0c75a6368c4c8ccd425ca73ebc92fcef963655ef
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-06-12 17:20:20 +0530

    Adding tailwind classes to make the app responsive.

--- src/Dicer.jsx
+++ src/Dicer.jsx
@@ -30,14 +30,14 @@ function Dicer() {
   }
 
   return (
-    <div className="bg-black w-2/5 text-red-400 flex flex-col justify-center items-center gap-14 p-14 rounded-2xl">
-      <h1 className='text-3xl font-semibold'>{winner}</h1>
-      <div className="dices w-full flex justify-evenly gap-12">
-        <div className="flex flex-col gap-4 w-full items-center">
+    <div className="bg-black w-4/5 md:3-/5 lg:w-2/5 text-red-400 flex flex-col justify-center items-center gap-6 md:gap-14 p-10 md:p-14 rounded-2xl">
+      <h1 className='text-3xl font-semibold text-center'>{winner}</h1>
+      <div className="dices w-full flex flex-col items-center md:flex-row justify-evenly gap-12">
+        <div className="flex flex-col gap-4 w-4/5 md:w-full items-center">
           <h2 className='text-2xl'>Player 1</h2>
           <img className='hover:rotate-180 duration-300' src={player1Dice} alt="Player 1 Dice" />
         </div>
-        <div className="flex flex-col gap-4 w-full items-center">
+        <div className="flex flex-col gap-4 w-4/5 md:w-full items-center">
           <h2 className='text-2xl'>Player 2</h2>
           <img className='hover:rotate-180 duration-300' src={player2Dice} alt="Player 2 Dice" />
         </div>
